ad2307cae5bfe6f4ad6f0019b894ef95af472721
dimirob <82870059+dimirob@users.noreply.github.com>
2024-01-20 19:24:42 +0200

    del

--- angular-Weather-Widget/scripts/build_prod.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-ng build --configuration=production --base-href=https://dimirob.github.io/
-rm -rf ../docs
-cp -r dist/dc-website ../docs
-cp ../docs/index.html ../docs/404.html
